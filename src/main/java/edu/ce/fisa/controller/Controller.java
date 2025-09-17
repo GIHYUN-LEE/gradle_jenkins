daddccf836335bd14a7e12af9cd4e9ba0c39a0cc
GIHYUN-LEE <gihyun506@naver.com>
2025-09-17 15:47:59 +0900

    please

--- src/main/java/edu/ce/fisa/controller/Controller.java
+++ src/main/java/edu/ce/fisa/controller/Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 	@GetMapping("/get")
 	public String getReqRes() {
-		return "This is TO-BE get methodwow!";
+		return "This is TO-BsdefE get methodwow!";
 	}
 	
 	//http://localhost:8088/app/post
