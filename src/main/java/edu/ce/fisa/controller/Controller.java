ab94b44f2147e8ec263aea3d2c108db7cd9c9a8c
GIHYUN-LEE <gihyun506@naver.com>
2025-09-17 23:30:50 +0900

    plz

--- src/main/java/edu/ce/fisa/controller/Controller.java
+++ src/main/java/edu/ce/fisa/controller/Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 	@GetMapping("/get")
 	public String getReqRes() {
-		return "Kang Hsefsefsefan dddkSol! !";
+		return "fuck!";
 	}
 	
 	//http://localhost:8088/app/post
