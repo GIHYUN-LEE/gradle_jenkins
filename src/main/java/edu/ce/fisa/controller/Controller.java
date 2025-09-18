96ae0040a0206b82ca2f26c8ae0816b5d0fc18ca
GIHYUN-LEE <gihyun506@naver.com>
2025-09-18 16:06:39 +0900

    what?

--- src/main/java/edu/ce/fisa/controller/Controller.java
+++ src/main/java/edu/ce/fisa/controller/Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 	@GetMapping("/get")
 	public String getReqRes() {
-		return "This is Get Method.";
+		return "Push DokerHub";
 	}
 	
 	//http://localhost:8088/app/post
